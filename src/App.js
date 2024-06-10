84816272f50b3868105edcd869cc913ef5511648
Denis Polyakov <pdd1328@gmail.com>
2024-06-11 02:03:46 +0300

    Закрыл вопросы по ревью

--- src/App.js
+++ src/App.js
@@ -4,22 +4,44 @@ import Categories from "./components/Categories/Categories";
 import Footer from "./components/Footer/Footer";
 import Participant from "./components/Participant/Participant";
 import Privelege from "./components/Privelege/Privelege";
+import clsx from "clsx";
 
 function App() {
-
   const component = [
-    {id: 1, name: Privelege},
-    {id: 2, name: Categories},
-    {id: 3, name: Participant},
-    {id: 4, name: Authorizacion},
-    {id: 5, name: Footer}
-  ]
+    { id: 1, name: Privelege },
+    { id: 2, name: Categories },
+    { id: 3, name: Participant },
+    { id: 4, name: Authorizacion },
+    { id: 5, name: Footer },
+  ];
 
   return (
     <div className="relative w-full h-full min-w-full min-h-full overflow-hidden text-lg">
       <main>
         {component.map((comp) => {
-          return (<section key={comp.id} className={`h-full ${comp.id === 1 && 'sm:h-screen bg-regal-blue sm:max-h-530 overflow-hidden'} ${comp.id === 4 && 'bg-white'} bg-gray flex items-center justify-center`}><div className={`max-w-1440 h-full w-full py-16 sm:py-0 px-12 sm:px-16 md:px-20 lg:px-32 xl:px-40 ${comp.id === 3 && 'max-sm:pt-0 sm:pb-[70px]'}`}><comp.name /></div></section>)
+          return (
+            <section
+              key={comp.id}
+              className={clsx(
+                "h-full bg-gray flex items-center justify-center",
+                {
+                  "sm:h-screen bg-regal-blue sm:max-h-530 overflow-hidden": comp.id === 1,
+                  "bg-white": comp.id === 4,
+                }
+              )}
+            >
+              <div
+                className={clsx(
+                  "max-w-1440 h-full w-full py-16 sm:py-0 px-12 sm:px-16 md:px-20 lg:px-32 xl:px-40",
+                  {
+                    "max-sm:pt-0 sm:pb-[70px]": comp.id === 3,
+                  }
+                )}
+              >
+                <comp.name />
+              </div>
+            </section>
+          );
         })}
       </main>
     </div>
--- src/components/Authorizacion/LoginForm/LoginForm.jsx
+++ src/components/Authorizacion/LoginForm/LoginForm.jsx
@@ -1,54 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import element from './customCheck.module.css';
-import arrowImg from './../../../assets/image/arrow.png';
-import lock from './../../../assets/image/lock.png';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import element from "./customCheck.module.css";
+import arrowImg from "./../../../assets/image/arrow.png";
+import lock from "./../../../assets/image/lock.png";
+import clsx from "clsx";
 
 // Пример объекта с именами пользователей и паролями
 const users = {
-  user1: 'password123',
-  user2: 'securepass456'
+  user1: "password123",
+  user2: "securepass456",
 };
 
 const LoginForm = () => {
-
-  const { register, handleSubmit, formState: { errors }, setError, getValues, reset, watch } = useForm({
-    mode: 'onChange'
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    setError,
+    reset,
+    getValues,
+  } = useForm({
+    mode: "onChange",
   });
 
   const [showPassword, setShowPassword] = useState(false);
-  const [isFormValid, setIsFormValid] = useState(false);
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     const { username, password } = data;
-    
+
     if (users[username] && users[username] === password) {
-      console.log('Вход выполнен успешно:', data);
+      console.log("Вход выполнен успешно:", data);
       // Логика успешного входа
       reset(); // Очистка формы после успешного входа
     } else {
-      setError('password', {
-        type: 'manual',
-        message: 'Неправильное имя пользователя или пароль'
+      setError("password", {
+        type: "manual",
+        message: "Неправильное имя пользователя или пароль",
       });
     }
   };
-
-  // Слежение за изменениями в полях формы
-  const watchFields = watch(['username', 'password', 'agreeTerms', 'receiveUpdates', 'ageConfirmation']);
-
-  useEffect(() => {
-    // Проверка на заполненность всех обязательных полей
-    setIsFormValid(
-      watchFields.every(field => field)
-    );
-  }, [watchFields]);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="max-w-[548px]">
@@ -57,37 +53,51 @@ const LoginForm = () => {
           id="username"
           type="text"
           placeholder="Логин"
-          className={`mt-1 placeholder:text-[18px] placeholder:text-descr-blue text-descr-blue block w-full px-[27.5px] py-[23px] border ${errors.username ? 'border-red-error' : 'border-bdr-gray'} rounded-md shadow-sm focus:outline-none`}
-          {...register('username', { 
-            required: 'Имя пользователя обязательно',
+          className={clsx(
+            "mt-1 placeholder:text-[18px] placeholder:text-descr-blue text-descr-blue block w-full px-[27.5px] py-[23px] border rounded-md shadow-sm focus:outline-none",
+            {
+              "border-red-error": errors.username,
+              "border-bdr-gray": !errors.username,
+            }
+          )}
+          {...register("username", {
+            required: "Имя пользователя обязательно",
             minLength: {
               value: 3,
-              message: 'Имя пользователя должно быть не менее 3 символов'
-            }
+              message: "Имя пользователя должно быть не менее 3 символов",
+            },
           })}
         />
-        {errors.username && <p className="mt-2 text-red-error">{errors.username.message}</p>}
+        {errors.username && (
+          <p className="mt-2 text-red-error">{errors.username.message}</p>
+        )}
       </div>
-      
+
       <div className="mb-[50px] relative">
         <input
           id="password"
-          type={showPassword ? 'text' : 'password'}
+          type={showPassword ? "text" : "password"}
           placeholder="Пароль"
-          className={`mt-1 placeholder:text-[18px] placeholder:text-descr-blue text-descr-blue block w-full px-[27.5px] py-[23px] border ${errors.password ? 'border-red-error' : 'border-bdr-gray'} rounded-md shadow-sm focus:outline-none`}
-          {...register('password', {
-            required: 'Пароль обязателен',
+          className={clsx(
+            "mt-1 placeholder:text-[18px] placeholder:text-descr-blue text-descr-blue block w-full px-[27.5px] py-[23px] border rounded-md shadow-sm focus:outline-none",
+            {
+              "border-red-error": errors.password,
+              "border-bdr-gray": !errors.password,
+            }
+          )}
+          {...register("password", {
+            required: "Пароль обязателен",
             minLength: {
               value: 6,
-              message: 'Пароль должен быть не менее 6 символов'
+              message: "Пароль должен быть не менее 6 символов",
             },
             validate: (value) => {
-              const username = getValues('username');
+              const username = getValues("username");
               if (!users[username] || users[username] !== value) {
-                return 'Неправильное имя пользователя или пароль';
+                return "Неправильное имя пользователя или пароль";
               }
               return true;
-            }
+            },
           })}
         />
         <button
@@ -95,66 +105,115 @@ const LoginForm = () => {
           className="absolute top-[13px] right-[18px] px-3 py-3 text-gray-600 focus:outline-none"
           onClick={togglePasswordVisibility}
         >
-          <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} color='gray' />
+          <FontAwesomeIcon
+            icon={showPassword ? faEyeSlash : faEye}
+            color="gray"
+          />
         </button>
-        {errors.password && <p className="mt-2 text-red-error">{errors.password.message}</p>}
+        {errors.password && (
+          <p className="mt-2 text-red-error">{errors.password.message}</p>
+        )}
       </div>
 
-      <span className='block border-t w-full border-bdr-gray mb-[23px]'></span>
+      <span className="block border-t w-full border-bdr-gray mb-[23px]"></span>
 
       <div className="mb-3">
         <label className="flex items-baseline text-descr-blue relative">
           <input
             type="checkbox"
-            className={(errors?.agreeTerms?.message ? "border-red-error " : "") + "appearance-none absolute top-0.5 left-0 m-0 w-[23px] h-[23px] border rounded-lg bg-gray-200 border-bdr-gray transition-all checked:bg-log-orange checked:border-log-orange " + element.element}
-            {...register('agreeTerms', { required: 'Принятие пользовательского соглашения обязательно' })}
+            className={clsx(
+              "appearance-none absolute top-0.5 left-0 m-0 w-[23px] h-[23px] border rounded-lg bg-gray-200 border-bdr-gray transition-all checked:bg-log-orange checked:border-log-orange",
+              element.element,
+              {
+                'border-red-error': errors?.agreeTerms?.message,
+              }
+            )}
+            {...register("agreeTerms", {
+              required: "Принятие пользовательского соглашения обязательно",
+            })}
           />
           <p className="ml-[35px]">
-            Принимаю <a href="#/" className="text-indigo-600 underline">пользовательское соглашение</a>
+            Принимаю{" "}
+            <a href="#/" className="text-indigo-600 underline">
+              пользовательское соглашение
+            </a>
           </p>
         </label>
-        {errors.agreeTerms && <p className="mt-2 text-red-error">{errors.agreeTerms.message}</p>}
+        {errors.agreeTerms && (
+          <p className="mt-2 text-red-error">{errors.agreeTerms.message}</p>
+        )}
       </div>
 
       <div className="mb-3">
         <label className="flex items-baseline text-descr-blue relative">
           <input
             type="checkbox"
-            className={(errors?.receiveUpdates?.message ? "border-red-error " : "") + "appearance-none absolute top-0.5 left-0 m-0 w-[23px] h-[23px] border rounded-lg bg-gray-200 border-bdr-gray transition-all checked:bg-log-orange checked:border-log-orange " + element.element}
-            {...register('receiveUpdates', { required: 'Соглашение на обработку и передачу персональных данных обязательно' })}
+            className={clsx(
+              "appearance-none absolute top-0.5 left-0 m-0 w-[23px] h-[23px] border rounded-lg bg-gray-200 border-bdr-gray transition-all checked:bg-log-orange checked:border-log-orange",
+              element.element,
+              {
+                'border-red-error': errors?.receiveUpdates?.message,
+              }
+            )}
+            {...register("receiveUpdates", {
+              required:
+                "Соглашение на обработку и передачу персональных данных обязательно",
+            })}
           />
           <p className="ml-[35px]">
-            Соглашаюсь на <a href="#/" className="text-indigo-600 underline">обработку и передачу персональных данных</a>
+            Соглашаюсь на{" "}
+            <a href="#/" className="text-indigo-600 underline">
+              обработку и передачу персональных данных
+            </a>
           </p>
         </label>
-        {errors.receiveUpdates && <p className="mt-2 text-red-error">{errors.receiveUpdates.message}</p>}
+        {errors.receiveUpdates && (
+          <p className="mt-2 text-red-error">{errors.receiveUpdates.message}</p>
+        )}
       </div>
 
       <div className="mb-[50px]">
         <label className="flex items-baseline text-descr-blue relative">
           <input
             type="checkbox"
-            className={(errors?.ageConfirmation?.message ? "border-red-error " : "") + "appearance-none absolute top-0.5 left-0 m-0 w-[23px] h-[23px] border rounded-lg bg-gray-200 border-bdr-gray transition-all checked:bg-log-orange checked:border-log-orange " + element.element}
-            {...register('ageConfirmation', { required: 'Принятие политики обработки персональных данных клиентов обязательно' })}
+            className={clsx(
+              "appearance-none absolute top-0.5 left-0 m-0 w-[23px] h-[23px] border rounded-lg bg-gray-200 border-bdr-gray transition-all checked:bg-log-orange checked:border-log-orange",
+              element.element,
+              {
+                'border-red-error': errors?.ageConfirmation?.message,
+              }
+            )}
+            {...register("ageConfirmation", {
+              required:
+                "Принятие политики обработки персональных данных клиентов обязательно",
+            })}
           />
           <p className="ml-[35px]">
-            Принимаю <a href="#/" className="text-indigo-600 underline">политику обработки персональных данных</a> клиентов
+            Принимаю{" "}
+            <a href="#/" className="text-indigo-600 underline">
+              политику обработки персональных данных
+            </a>{" "}
+            клиентов
           </p>
         </label>
-        {errors.ageConfirmation && <p className="mt-2 text-red-error">{errors.ageConfirmation.message}</p>}
+        {errors.ageConfirmation && (
+          <p className="mt-2 text-red-error">
+            {errors.ageConfirmation.message}
+          </p>
+        )}
       </div>
-      
+
       <button
         type="submit"
         className="w-full p-[34px] flex justify-between items-center text-white font-medium rounded-lg bg-log-orange focus:outline-none disabled:bg-btn-gray disabled:cursor-not-allowed"
-        disabled={!isFormValid}
+        disabled={!isValid}
       >
         Продолжить
         <img src={arrowImg} alt="arrow" />
       </button>
-      <div className='flex items-start mt-[25px]'>
-        <img src={lock} alt="lock" className='pt-[3px]' />
-        <p className='ml-[15px] text-descr-blue'> 
+      <div className="flex items-start mt-[25px]">
+        <img src={lock} alt="lock" className="pt-[3px]" />
+        <p className="ml-[15px] text-descr-blue">
           Используем шифрование для гарантии безопасности персональных данных
         </p>
       </div>
